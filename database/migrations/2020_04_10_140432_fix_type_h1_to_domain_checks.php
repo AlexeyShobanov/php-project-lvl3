cdf210c98c937ec8099f576b9c22f0971811a8a0
AlexeyShobanov <shobanov@gmail.com>
2020-04-10 14:20:15 +0000

    fix db h1 type

---
+++ database/migrations/2020_04_10_140432_fix_type_h1_to_domain_checks.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixTypeH1ToDomainChecks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('domain_checks', function (Blueprint $table) {
+            $table->text('h1')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('domain_checks', function (Blueprint $table) {
+            $table->string('h1')->change();
+        });
+    }
+}
